baeb1c455b95175c35710eae66c161fe4e6368c8
Fang Liu <lfxjtu@gmail.com>
2022-09-25 15:21:08 +1300

    add RestController

--- src/main/java/io/lfxjtu/springbootstarter/hello/HelloController.java
+++ src/main/java/io/lfxjtu/springbootstarter/hello/HelloController.java
@@ -1,12 +1,13 @@
 package io.lfxjtu.springbootstarter.hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @RequestMapping(path = "/hello",method = RequestMethod.GET)
     public String sayHi(){
         return "Hi!";
     }
