38d89ca1275dab3571b1844181813332ee8a9b73
Fang Liu <lfxjtu@gmail.com>
2022-10-02 12:22:33 +1300

    add nullable exception handling

--- src/main/java/io/lfxjtu/springbootstarter/topic/TopicService.java
+++ src/main/java/io/lfxjtu/springbootstarter/topic/TopicService.java
@@ -4,22 +4,24 @@ import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics = Arrays.asList(
+    private final List<Topic> topics = Arrays.asList(
             new Topic("spring", "Spring Framework", "Spring Framework Description"),
             new Topic("java", "Java Core", "Java Core Description"),
             new Topic("javascript", "Javascript name", "Javascript Description")
     );
 
-    public List<Topic> getAllTopics(){
+    public List<Topic> getAllTopics() {
         return topics;
     }
 
-    public Topic getTopic(String id){
-        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    public Topic getTopic(String id) {
+        Optional<Topic> topic = topics.stream().filter(t ->t.getId().equals(id)).findFirst();
+        return topic.orElse(null);
     }
 }
 
